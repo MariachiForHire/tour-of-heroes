327848d16d66af3bd45366ed8f193dd8560d849d
MariachiForHire <condadom@gmail.com>
2021-03-10 14:02:37 -0500

    adding navigation funcs

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,23 +10,16 @@ import { MessageService } from '../message.service'
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero?: Hero;
+  heroes: Hero[];
 
-  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
+
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
